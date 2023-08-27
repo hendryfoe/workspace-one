f54e2862231a7c20bbc0a1a5fc558088973d8720
hendryfoe <hendryanto.fudiko@gmail.com>
2023-08-27 23:58:17 +0700

    feat: add home page

--- libs/components/navbar.tsx
+++ libs/components/navbar.tsx
@@ -5,7 +5,7 @@ export function Navbar() {
     <>
       <div className="flex flex-wrap">
         <section className="relative w-full">
-          <nav className="flex justify-between bg-gray-900 text-white"></nav>
+          <nav className="flex justify-between bg-gray-900 text-white">&nbsp;</nav>
         </section>
       </div>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,119 +1,81 @@
-import Image from 'next/image';
+'use client';
 
+import React from 'react';
+
+import { ChartSection, transformToTVChartData } from '@/components/chart-section';
+import { BinanceTrades, MarketTrade, TRADES_LIMIT, transformToTradeTableData } from '@/components/market-trade';
 import { Navbar } from '@/components/navbar';
+import { Spinner } from '@/components/spinner';
+import { BinanceUiKLines } from '@/models/binance';
+import { FETCH_ITEMS_LIMIT, getTradesBinanceAPI, getUiKLinesBinanceAPI } from '@/utils/api';
 
-import styles from '../styles/root.module.css';
+async function getData() {
+  const promises = [
+    getUiKLinesBinanceAPI({
+      symbol: 'BTCUSDT',
+      endTime: Date.now().toString(),
+      interval: '1d',
+      limit: FETCH_ITEMS_LIMIT
+    }),
+    getTradesBinanceAPI({
+      symbol: 'BTCUSDT',
+      limit: TRADES_LIMIT.toString()
+    })
+  ];
+  const [uiKLinesResult, tradesData] = await Promise.all(promises);
+  const chartData = transformToTVChartData(uiKLinesResult as BinanceUiKLines);
+  const tradesTableData = transformToTradeTableData(tradesData as BinanceTrades);
 
-export default async function Page() {
+  return {
+    chartData,
+    tradesTableData
+  };
+}
+
+export default function Page() {
+  // const data = await getData();
+  const [data, setData] = React.useState<Awaited<ReturnType<typeof getData>>>({
+    chartData: {
+      areas: [],
+      candlesticks: []
+    },
+    tradesTableData: []
+  });
+
+  React.useEffect(() => {
+    let isCancel = false;
+
+    getData().then((result) => {
+      if (!isCancel) {
+        setData(result);
+      }
+    });
+    return () => {
+      isCancel = true;
+    };
+  }, []);
+
   return (
     <div className="w-full h-screen">
       <Navbar />
 
-      <main>
-        <div className="w-full bg-gray-800">
-          <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
-            <div className="text-center pb-12">
-              <h2 className="text-base font-bold text-indigo-600">We have the best equipment in the market</h2>
-              <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-white">
-                Check our awesome team membwhite
-              </h1>
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-                <div>
-                  <img
-                    className="object-center object-cover h-auto w-full"
-                    src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-                    alt="photo"
-                  />
-                </div>
-                <div className="text-center py-8 sm:py-6">
-                  <p className="text-xl text-white font-bold mb-2">Dany Bailey</p>
-                  <p className="text-base text-gray-400 font-normal">Software Engineer</p>
-                </div>
-              </div>
-              <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-                <div>
-                  <img
-                    className="object-center object-cover h-auto w-full"
-                    src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-                    alt="photo"
-                  />
-                </div>
-                <div className="text-center py-8 sm:py-6">
-                  <p className="text-xl text-white font-bold mb-2">Lucy Carter</p>
-                  <p className="text-base text-gray-400 font-normal">Graphic Designer</p>
-                </div>
-              </div>
-              <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-                <div>
-                  <img
-                    className="object-center object-cover h-auto w-full"
-                    src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80"
-                    alt="photo"
-                  />
-                </div>
-                <div className="text-center py-8 sm:py-6">
-                  <p className="text-xl text-white font-bold mb-2">Jade Bradley</p>
-                  <p className="text-base text-gray-400 font-normal">Dev Ops</p>
-                </div>
-              </div>
-              <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-                <div>
-                  <img
-                    className="object-center object-cover h-auto w-full"
-                    src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-                    alt="photo"
-                  />
-                </div>
-                <div className="text-center py-8 sm:py-6">
-                  <p className="text-xl text-white font-bold mb-2">Dany Bailey</p>
-                  <p className="text-base text-gray-400 font-normal">Software Engineer</p>
-                </div>
-              </div>
-              <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-                <div>
-                  <img
-                    className="object-center object-cover h-auto w-full"
-                    src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-                    alt="photo"
-                  />
-                </div>
-                <div className="text-center py-8 sm:py-6">
-                  <p className="text-xl text-white font-bold mb-2">Lucy Carter</p>
-                  <p className="text-base text-gray-400 font-normal">Graphic Designer</p>
-                </div>
-              </div>
-              <div className="w-full bg-gray-900 rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-                <div>
-                  <img
-                    className="object-center object-cover h-auto w-full"
-                    src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80"
-                    alt="photo"
-                  />
-                </div>
-                <div className="text-center py-8 sm:py-6">
-                  <p className="text-xl text-white font-bold mb-2">Jade Bradley</p>
-                  <p className="text-base text-gray-400 font-normal">Dev Ops</p>
-                </div>
-              </div>
-            </div>
-          </section>
+      {data.chartData.candlesticks.length === 0 && (
+        <div className="h-full mt-52">
+          <Spinner />
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      )}
+      {data.chartData.candlesticks.length > 0 && (
+        <main className="mt-4 container mx-auto">
+          <section className="w-full grid grid-cols-[1fr_minmax(300px,400px)] gap-4">
+            <section>
+              <ChartSection data={data.chartData} />
+            </section>
+            <section>
+              <MarketTrade tableData={data.tradesTableData} />
+            </section>
+          </section>
+        </main>
+      )}
     </div>
   );
 }
